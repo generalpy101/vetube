2a4b61bc85de92ee29c84cabc858b70288753632
Prakash Yogi <48615112+generalpy101@users.noreply.github.com>
2024-01-31 14:15:52 +0530

    Update src/web/config.py
    Co-authored-by: code-review-doctor[bot] <72320148+code-review-doctor[bot]@users.noreply.github.com>

--- src/web/config.py
+++ src/web/config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, ".env"))
 
 
-class Config(object):
+class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret"
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
